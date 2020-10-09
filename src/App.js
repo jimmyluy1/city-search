6c6935fd76dd45438eddddab99d36a8fa7778fa4
jimmyluy1 <luyjimmy037@gmail.com>
2020-10-09 13:31:48 -0400

    added city search
    Signed-off-by: jimmyluy1 <luyjimmy037@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+
+
+function City(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <ul>
+      <li>ZipCode:{props.data}</li>
+    </ul>
+</div>
   );
 }
+
+function CitySearchField(props) {
+  return ( 
+  <div>
+    Type in a city:
+    <input type="text" onChange={ (e) => props.cityChanged(e)} value={props.value}/>
+    <p>The city entered is: {props.value} </p>
+    </div>
+    );
+}
+
+
+
+class App extends Component {
+  state = {
+      userInputValue: "",
+      zipCodes: [],
+  };
+
+
+ 
+  
+
+  handleCityChange(event){
+    this.setState({
+      userInputValue: event.target.userInputValue,
+    })
+
+    
+    fetch("http://ctp-zip-api.herokuapp.com/city/" + event.target.value)
+    .then(res => res.json())
+    .then(jsonData => {
+      this.setState({
+        zipCodes: jsonData
+      });
+    })
+    .catch(err => this.setState({cities: [] }))
+  
+}
+
+  render() {
+    console.log(this.state.cities);
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2> City Search</h2>
+        </div >
+        
+        <CitySearchField class="uppercase" cityChanged ={(e)=> this.handleCityChange(e)} value={this.state.onChange}/>
+        
+        <div>
+          { this.state.zipCodes.map((zipCode) => {
+            return <City data={zipCode}/>;
+           }) }
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default App;
